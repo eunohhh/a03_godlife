81d82623995e4e8464f5761679a94d030df1b561
Eun Oh <bdohhhhh@gmail.com>
2024-07-08 19:44:15 +0900

    240708 [script] : tailwind config 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,9 @@ const config = {
     ],
     prefix: "",
     theme: {
+        screens: {
+            mobile: "440px",
+        },
         container: {
             center: true,
             padding: "2rem",
