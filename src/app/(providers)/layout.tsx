d19e3172c5cee4378a487e6eefd3e17c6a0017d1
Eun Oh <bdohhhhh@gmail.com>
2024-07-08 17:52:50 +0900

    240708 [add] : 프로젝트 기본 구조 설정

---
+++ src/app/(providers)/layout.tsx
@@ -0,0 +1,16 @@
+import { AuthProvider } from "@/context/auth.context";
+import { createClient } from "@/supabase/server";
+import React from "react";
+
+async function ProvidersLayout({ children }: { children: React.ReactNode }) {
+    const supabase = createClient();
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    const me = user || null;
+
+    return <AuthProvider initialMe={me}>{children}</AuthProvider>;
+}
+
+export default ProvidersLayout;
