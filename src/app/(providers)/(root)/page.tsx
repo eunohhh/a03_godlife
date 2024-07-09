df4c29d6b05a30f54605816e3731f75c86500ee7
RyanYang99 <diddbstjd0220@naver.com>
2024-07-09 16:36:04 +0900

    "[style] : select,avatar,separator shadcn/ui 이용" "[add] : MainPost component 파일 생성해서 supabase 연결"

--- src/app/(providers)/(root)/page.tsx
+++ src/app/(providers)/(root)/page.tsx
@@ -1,5 +1,47 @@
+import MainPost from "@/components/MainPost";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/Avatar";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/Select";
+
 function MainPage() {
-    return <div>이곳에 사이드바가 위치합니다.</div>;
+  return (
+    <div className="container w-full bg-turtleGreen max-w-[428px] p-2">
+      <div className="navbar-center bg-#1d1d1d w-full h-[60px] text-center pt-[1rem] border-gray-500 border-b-2">
+        <div className="text-white">Main Header</div>
+      </div>
+      <div className="flex flex-row justify-between px-2 my-5">
+        <Avatar className="flex">
+          <AvatarImage
+            src="https://ngtnbcqokvtyrilhkwpz.supabase.co/storage/v1/object/public/profile/Vector.png"
+            alt="@shadcn"
+          />
+          <AvatarFallback>NA</AvatarFallback>
+        </Avatar>
+        <Avatar className="flex">
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>NA</AvatarFallback>
+        </Avatar>
+        <div className="flex">
+          <Select>
+            <SelectTrigger className="w-[75px]">
+              <SelectValue placeholder="정렬" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">인기순</SelectItem>
+              <SelectItem value="dark">최신순</SelectItem>
+              <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <MainPost></MainPost>
+    </div>
+  );
 }
 
 export default MainPage;
