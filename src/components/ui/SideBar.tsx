e690f2dc968467fdd43536e7dd95382edb877a5d
JJQKR <yjcoyjco@naver.com>
2024-07-09 13:37:15 +0900

    [add] Sheet.tsx 파일 추가

--- src/components/ui/SideBar.tsx
+++ src/components/ui/SideBar.tsx
@@ -1,10 +1,64 @@
 import React from "react";
-import { Drawer } from "./Drawer";
+import {
+  Drawer,
+  DrawerTrigger,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerFooter,
+  DrawerTitle,
+  DrawerDescription,
+} from "./Drawer";
+
+import {
+  Sheet,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetDescription,
+} from "./Sheet";
 
 const SideBar = () => {
   return (
     <>
-      <div>SideBar</div>
+      <Drawer>
+        <DrawerTrigger asChild>
+          <button>Open Drawer 버튼</button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Drawer Title</DrawerTitle>
+          </DrawerHeader>
+          <DrawerDescription>This is the drawer description.</DrawerDescription>
+          <div>SideBar</div>
+          <DrawerFooter>
+            <DrawerClose asChild>
+              <button>Close</button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+
+      <Sheet>
+        y
+        <SheetTrigger asChild>
+          <button>Open Sheet 버튼</button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <DrawerTitle>Drawer Title</DrawerTitle>
+          </SheetHeader>
+          <SheetDescription>This is the drawer description.</SheetDescription>
+          <div>SideBar</div>
+          <SheetFooter>
+            <SheetClose asChild>
+              <button>Close</button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
     </>
   );
 };
