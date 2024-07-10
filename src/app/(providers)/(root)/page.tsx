c0ddbe944e12c716198a636d513af84c8a08d9e9
RyanYang99 <diddbstjd0220@naver.com>
2024-07-10 16:17:12 +0900

    [update] : MainPage 오른쪽 하단에 TopButton 생성 및 클릭시 페이지 상단으로 이동하는 기능 구현

--- src/app/(providers)/(root)/page.tsx
+++ src/app/(providers)/(root)/page.tsx
@@ -16,6 +16,13 @@ function MainPage() {
     window.location.reload();
   };
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
       <div>
@@ -56,6 +63,14 @@ function MainPage() {
           </div>
         </div>
         <MainPost></MainPost>
+        <div className="fixed bottom-[5%] right-[35%] group">
+          <img
+            className="cursor-pointer transition-transform duration-300 ease-in-out transform group-hover:scale-110"
+            src="top_btn.svg"
+            alt="Top Button"
+            onClick={scrollToTop}
+          />
+        </div>
       </div>
     </>
   );
