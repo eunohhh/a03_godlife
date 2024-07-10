ec9f4d177686975baa880cf74b3c3a00caeb25cc
doeheekim <kimamy20@gmail.com>
2024-07-10 17:21:35 +0900

    [add] : 게시물 작성/수정 폼 컴포넌트 생성

---
+++ src/components/write/CreateOrEditForm.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function CreateOrEditForm() {
+  return (
+    <form className="bg-white p-6 pt-[60px] rounded-lg shadow-lg w-full max-w-[428px] min-h-[860px] flex flex-col">
+        <div className="flex justify-between items-center mb-4">
+          <Link
+            href="/"
+            className="text-turtleGreen hover:text-green-700 cursor-pointer w-[67px] h-[34px]"
+          >
+            Cancel
+          </Link>
+          <Image
+            src="/post_btn.svg"
+            alt="Post"
+            width={67}
+            height={34}
+            className=" hover:bg-green-700 cursor-pointer"
+          />
+        </div>
+        <textarea
+          className="w-full p-2 mb-4 border-none outline-none resize-none flex-grow"
+          placeholder="무슨 일이 일어나고 있나요?"
+        ></textarea>
+        <Image src="/big_logo.svg" alt="logo" width={100} height={100} className="mx-auto my-0" />
+        <div className="flex justify-between items-center mt-4">
+          <div className="text-turtleGreen">{0}/280</div>
+          <Image
+            src="/Image_upload_btn.svg"
+            alt="Image Uploader"
+            width={67}
+            height={34}
+            className=" hover:bg-green-700 cursor-pointer"
+          />
+        </div>
+      </form>
+  )
+}
--- src/zustand/store.ts
+++
@@ -1,8 +0,0 @@
-import supabase from "@/supabase/client";
-import { User } from "@/types/post.type";
-import { Database } from "@/types/supabase";
-import { create } from 'zustand';
-
-export const useStore = create((set) => ({
-
-}));
