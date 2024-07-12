d86756b0c52a696044aee8655e70c3655e730fd2
Eun Oh <bdohhhhh@gmail.com>
2024-07-12 15:07:34 +0900

    240712 [chore] : console 주석

--- src/components/ui/BasicLoader.tsx
+++ src/components/ui/BasicLoader.tsx
@@ -7,7 +7,7 @@ type BasicLoaderProps = {
 
 function BasicLoader({ isSmall = false }: BasicLoaderProps) {
     return (
-        <div className={clsx("relative w-[1024px]", isSmall ? "h-[50vh]" : "h-dvh")}>
+        <div className={clsx("relative w-[428px] my-0 mx-auto", isSmall ? "h-[40vh]" : "h-dvh")}>
             <div className="relative aspect-square w-[96px] h-[96px] top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2">
                 <Image
                     className="object-contain animate-bounce"
