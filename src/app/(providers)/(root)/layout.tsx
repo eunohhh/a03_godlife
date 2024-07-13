fd5b437ad1033eac0acd6ac3f9aebcde5d5a1661
Eun Oh <bdohhhhh@gmail.com>
2024-07-14 02:50:10 +0900

    240713 [bugfix] : 6차 완료

--- src/app/(providers)/(root)/layout.tsx
+++ src/app/(providers)/(root)/layout.tsx
@@ -1,11 +1,11 @@
-import React, { Suspense } from "react";
-import Loading from "./loading";
+import React from "react";
 
 function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <main className="w-dvw overflow-hidden">
             <div className="my-0 mx-auto bg-turtleGreen max-w-[428px]">
-                <Suspense fallback={<Loading />}>{children}</Suspense>
+                {/* <Suspense fallback={<Loading />}>{children}</Suspense> */}
+                {children}
             </div>
         </main>
     );
--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,10 +1,11 @@
+"use client";
 import { AuthContext } from "@/context/auth.context";
 import { useContext } from "react";
 
 export const useAuth = () => {
     const context = useContext(AuthContext);
 
-    if (context === null) {
+    if (context === undefined) {
         throw new Error("오류 발생! 오류발생! 훅은 프로바이더 안에서 써줘요잉");
     }
 
