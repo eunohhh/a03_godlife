3ea658a321f5a5a6bc79bfab0e557afda818d380
JJQKR <yjcoyjco@naver.com>
2024-07-13 21:01:39 +0900

    [fix] MainHeader.tsx의 텍스트를 God Life Mate로 수정, text-lg 추가

--- src/components/ui/BasicLoader.tsx
+++ src/components/ui/BasicLoader.tsx
@@ -2,25 +2,30 @@ import clsx from "clsx";
 import Image from "next/image";
 
 type BasicLoaderProps = {
-    isSmall?: boolean;
+  isSmall?: boolean;
 };
 
 function BasicLoader({ isSmall = false }: BasicLoaderProps) {
-    return (
-        <div className={clsx("relative w-[428px] my-0 mx-auto", isSmall ? "h-[40vh]" : "h-dvh")}>
-            <div className="relative aspect-square w-[96px] h-[96px] top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2">
-                <Image
-                    className="object-contain animate-bounce"
-                    src="/turtle.svg"
-                    alt="turtle"
-                    fill
-                    sizes="(min-width: 640px) 96px, 96px"
-                    priority
-                    unoptimized
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className={clsx(
+        "relative w-[428px] my-0 mx-auto",
+        isSmall ? "h-[40vh]" : "h-dvh"
+      )}
+    >
+      <div className="relative aspect-square w-[96px] h-[96px] top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2">
+        <Image
+          className="object-contain animate-bounce"
+          src="/turtle.svg"
+          alt="turtle"
+          fill
+          sizes="(min-width: 640px) 96px, 96px"
+          priority
+          unoptimized
+        />
+      </div>
+    </div>
+  );
 }
 
 export default BasicLoader;
