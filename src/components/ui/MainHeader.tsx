3f2a7596f3884a1b9bbf65efb67fb9c769bfdfd2
JJQKR <yjcoyjco@naver.com>
2024-07-14 13:29:14 +0900

    [update] MainHeader에 반갑습니다, 로그인하기 추가중

--- src/components/ui/MainHeader.tsx
+++ src/components/ui/MainHeader.tsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { useAuth } from "@/context/auth.context";
+import Link from "next/link";
+
 function MainHeader() {
+  const { me } = useAuth();
   return (
     <div className="navbar-center bg-#1d1d1d w-full h-[60px] text-center pt-[1rem] border-gray-500 border-b-2">
       <div className="text-white text-2xl font-MangoByeolbyeol">
         God Life Mate
       </div>
+      <p className="text-sm font-Pretendard-Regular text-gray-500">
+        {me ? "반갑습니다!" : <Link href="/"> 로그인하기</Link>}
+      </p>
     </div>
   );
 }
