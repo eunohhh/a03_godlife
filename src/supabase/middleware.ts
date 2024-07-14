1f97623876b830b705872cb51a7cff685c9dde02
sseul <hanbat107@naver.com>
2024-07-15 02:32:12 +0900

    [pull]

--- src/supabase/middleware.ts
+++ src/supabase/middleware.ts
@@ -3,80 +3,82 @@ import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function updateSession(request: NextRequest) {
-    // console.log("request 그 자체 =>", request);
-    let supabaseResponse = NextResponse.next({
-        request,
-    });
+  // console.log("request 그 자체 =>", request);
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
 
-    // console.log("first supabaseResponse =>", supabaseResponse);
+  // console.log("first supabaseResponse =>", supabaseResponse);
 
-    const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        {
-            cookies: {
-                getAll() {
-                    return request.cookies.getAll();
-                },
-                setAll(cookiesToSet) {
-                    // cookiesArray.forEach(({ name, value }) => request.cookies.set(name, value));
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          // cookiesArray.forEach(({ name, value }) => request.cookies.set(name, value));
 
-                    cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
-                    supabaseResponse = NextResponse.next({
-                        request,
-                    });
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value)
+          );
+          supabaseResponse = NextResponse.next({
+            request,
+          });
 
-                    // cookiesArray.forEach(({ name, value }) => {
-                    //     console.log("cookiesToSet ====>", name, value);
-                    //     supabaseResponse.cookies.set(name, value);
-                    // });
-                    cookiesToSet.forEach(({ name, value, options }) => {
-                        supabaseResponse.cookies.set(name, value, options);
-                    });
-                },
-            },
-        }
-    );
-
-    // IMPORTANT: Avoid writing any logic between createServerClient and
-    // supabase.auth.getUser(). A simple mistake could make it very hard to debug
-    // issues with users being randomly logged out.
-    const {
-        data: { user },
-    } = await supabase.auth.getUser();
-
-    // console.log("미들웨어에서 유저 =>", user);
-
-    // 유저가 없으면서, api, login, recover 를 제외한 라우트는 무조건 login 으로 리다이렉트
-    if (
-        !user &&
-        request.nextUrl.pathname !== "/" &&
-        !request.nextUrl.pathname.startsWith("/api") &&
-        !request.nextUrl.pathname.startsWith("/login") &&
-        !request.nextUrl.pathname.startsWith("/recover") &&
-        !request.nextUrl.pathname.startsWith("/signup")
-        // &&
-        // // 개발용으로 아래 추가 추후 삭제할 것
-        // !request.nextUrl.pathname.startsWith("/write") &&
-        // !request.nextUrl.pathname.startsWith("/profile")
-    ) {
-        // no user, potentially respond by redirecting the user to the login page
-        const url = request.nextUrl.clone();
-        url.pathname = "/login";
-        return NextResponse.redirect(url);
+          // cookiesArray.forEach(({ name, value }) => {
+          //     console.log("cookiesToSet ====>", name, value);
+          //     supabaseResponse.cookies.set(name, value);
+          // });
+          cookiesToSet.forEach(({ name, value, options }) => {
+            supabaseResponse.cookies.set(name, value, options);
+          });
+        },
+      },
     }
+  );
 
-    // 유저가 있을 때, login, signup 은 무조건 홈으로 리다이렉트
-    if (
-        (request.nextUrl.pathname.startsWith("/login") && user) ||
-        (request.nextUrl.pathname.startsWith("/signup") && user)
-    ) {
-        const url = request.nextUrl.clone();
-        url.pathname = "/";
-        return NextResponse.redirect(url);
-    }
+  // IMPORTANT: Avoid writing any logic between createServerClient and
+  // supabase.auth.getUser(). A simple mistake could make it very hard to debug
+  // issues with users being randomly logged out.
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-    // console.log("last supabaseResponse =>", supabaseResponse);
+  // console.log("미들웨어에서 유저 =>", user);
 
-    return supabaseResponse;
+  // 유저가 없으면서, api, login, recover 를 제외한 라우트는 무조건 login 으로 리다이렉트
+  if (
+    !user &&
+    request.nextUrl.pathname !== "/" &&
+    !request.nextUrl.pathname.startsWith("/api") &&
+    !request.nextUrl.pathname.startsWith("/login") &&
+    !request.nextUrl.pathname.startsWith("/recover") &&
+    !request.nextUrl.pathname.startsWith("/signup")
+    // &&
+    // // 개발용으로 아래 추가 추후 삭제할 것
+    // !request.nextUrl.pathname.startsWith("/write") &&
+    // !request.nextUrl.pathname.startsWith("/profile")
+  ) {
+    // no user, potentially respond by redirecting the user to the login page
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  // 유저가 있을 때, login, signup 은 무조건 홈으로 리다이렉트
+  if (
+    (request.nextUrl.pathname.startsWith("/login") && user) ||
+    (request.nextUrl.pathname.startsWith("/signup") && user)
+  ) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/";
+    return NextResponse.redirect(url);
+  }
+
+  // console.log("last supabaseResponse =>", supabaseResponse);
+
+  return supabaseResponse;
 }
