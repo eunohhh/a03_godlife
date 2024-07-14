1fbc90f25d6122a06328ebc17f77f55c228c5298
Eun Oh <bdohhhhh@gmail.com>
2024-07-14 19:56:25 +0900

    240714 [bugfix] : hydration 에러 버그픽스 시도

--- src/components/ui/WeatherData.tsx
+++ src/components/ui/WeatherData.tsx
@@ -4,32 +4,35 @@ import { Weather } from "@/types/weather";
 import React, { useEffect } from "react";
 
 interface WeatherDataProps {
-  onWeatherData: (
-    weather: Weather["weather"],
-    temp: number,
-    tempMin: number,
-    tempMax: number,
-    humidity: number
-  ) => void;
+    onWeatherData: (
+        weather: Weather["weather"],
+        temp: number,
+        tempMin: number,
+        tempMax: number,
+        humidity: number
+    ) => void;
 }
 
 const WeatherData: React.FC<WeatherDataProps> = ({ onWeatherData }) => {
-  useEffect(() => {
-    const fetchWeatherData = async () => {
-      const response = await fetch("/api/weather");
-      const data: Weather = await response.json();
+    useEffect(() => {
+        const fetchWeatherData = async () => {
+            const response = await fetch("/api/weather", {
+                method: "GET",
+                cache: "no-store",
+            });
+            const data: Weather = await response.json();
 
-      onWeatherData(
-        data.weather,
-        data.main.temp,
-        data.main.temp_min,
-        data.main.temp_max,
-        data.main.humidity
-      );
-    };
-    fetchWeatherData();
-  }, []);
-  return null;
+            onWeatherData(
+                data.weather,
+                data.main.temp,
+                data.main.temp_min,
+                data.main.temp_max,
+                data.main.humidity
+            );
+        };
+        fetchWeatherData();
+    }, []);
+    return null;
 };
 
 export default WeatherData;
