1700759a660ef7a372a4905a7366334259483216
JJQKR <yjcoyjco@naver.com>
2024-07-14 14:31:03 +0900

    [update] 메인 헤더 로그인, 로그아웃 추가

--- src/components/ui/MainHeader.tsx
+++ src/components/ui/MainHeader.tsx
@@ -4,14 +4,18 @@ import { useAuth } from "@/context/auth.context";
 import Link from "next/link";
 
 function MainHeader() {
-  const { me } = useAuth();
+  const { me, logOut } = useAuth();
   return (
     <div className="navbar-center bg-#1d1d1d w-full h-[60px] text-center pt-[1rem] border-gray-500 border-b-2">
       <div className="text-white text-2xl font-MangoByeolbyeol">
         God Life Mate
       </div>
-      <p className="text-sm font-Pretendard-Regular text-gray-500">
-        {me ? "반갑습니다!" : <Link href="/"> 로그인하기</Link>}
+      <p className="text-[10px] mt-4 font-Pretendard-Regular text-gray-500">
+        {me ? (
+          <button onClick={logOut}>로그아웃</button>
+        ) : (
+          <Link href="/login"> 로그인</Link>
+        )}
       </p>
     </div>
   );
