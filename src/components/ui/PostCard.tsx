e158346ff2971a3d6475d55a550871489168e84e
doeheekim <kimamy20@gmail.com>
2024-07-15 10:37:51 +0900

    [update] 게시물 수정 페이지 기능 구현 완료

--- src/components/ui/PostCard.tsx
+++ src/components/ui/PostCard.tsx
@@ -13,86 +13,82 @@ import { Avatar, AvatarFallback, AvatarImage } from "./Avatar";
 import { Separator } from "./Separator";
 
 function PostCard({ post }: { post: Post }) {
-    // const { me } = useAuth();
+  // const { me } = useAuth();
 
-    const { data, isPending: userIsPending, error: userError } = useMeQuery();
-    const me = data?.userTableInfo;
+  const { data, isPending: userIsPending, error: userError } = useMeQuery();
+  const me = data?.userTableInfo;
 
-    const pathname = usePathname();
-    const router = useRouter();
+  const pathname = usePathname();
+  const router = useRouter();
 
-    const handleDelete = async () => {
-        console.log(post.id);
+  const handleDelete = async () => {
+    if (!me) return;
+    if (post.email !== me.email) {
+      return showAlert("caution", "게시물 작성자만 삭제할 수 있어요!");
+    }
 
-        if (!me) return;
-        if (post.email !== me.email) {
-            return showAlert("caution", "게시물 작성자만 삭제할 수 있어요!");
-        }
+    const isConfirmed = confirm("게시물을 삭제할까요?");
+    if (!isConfirmed) return;
 
-        const isConfirmed = confirm("게시물을 삭제할까요?");
-        if (!isConfirmed) return;
+    const result = await deletePost(post.id);
+    if (result) {
+      showAlert("success", "게시물이 삭제되었어요!");
+      return router.refresh();
+    } else {
+      return showAlert("error", "앗! 게시물 삭제에 실패했어요..");
+    }
+  };
 
-        const result = await deletePost(post.id);
-        if (result) {
-            router.refresh();
-            console.log(post.id, "삭제 성공 좀 돼라");
-        } else {
-            console.log("삭제 실패");
-            // 에러 처리
-        }
-    };
-
-    return (
-        <div className="post-card max-h-[200px] bg-white rounded-lg p-5 ">
+  return (
+    <div className="post-card max-h-[200px] bg-white rounded-lg p-5 ">
+      <div className="flex flex-row">
+        <div>
+          {me?.email === post.email && pathname === "/profile" && (
             <div className="flex flex-row">
-                {/* 여기에 수정삭제 */}
-
-                <div>
-                    {me?.email === post.email && pathname === "/profile" && (
-                        <div className="flex flex-row">
-                            {/* 완성되면 edit 로 수정*/}
-                            <Link href={`/write`}>수정</Link>
-                            <button onClick={handleDelete}>삭제</button>
-                        </div>
-                    )}
-                </div>
-
-                <Avatar className="flex">
-                    <AvatarImage src={post.avatar!} alt="@profile" />
-                    <AvatarFallback>
-                        <div className="h-10 w-10 relative rounded-full overflow-hidden">
-                            <Image
-                                src={
-                                    "https://ngtnbcqokvtyrilhkwpz.supabase.co/storage/v1/object/public/profile/Vector.png"
-                                }
-                                alt="profile"
-                                fill
-                                className="object-contain"
-                                sizes="(max-width: 768px) 100vw, 33vw"
-                            />
-                        </div>
-                    </AvatarFallback>
-                </Avatar>
-
-                <div className="flex flex-col card content ml-5 w-full">
-                    <div className="flex items-center">
-                        <h4 className="text-sm font-medium leading-none mr-2">{post.nickname}</h4>
-                        <p className="text-sm text-muted-foreground">{post.email}</p>
-                    </div>
-                    <p className="text-xs text-muted-foreground mt-2">
-                        {format(new Date(post.created_at), "yyyy-MM-dd HH:mm")}
-                    </p>
-                    <Separator className="mt-1 mb-6 border-black" />
-                    <div className="flex h-5 items-center space-x-4 text-sm">
-                        <p className="text-ellipsis line-clamp-3 overflow-hidden w-[80%]">
-                            {post.contents}
-                        </p>
-                    </div>
-                    <CheerupButton postId={post.id} />
-                </div>
+              <Link href={`/edit/${post.id}`}>수정</Link>
+              <button onClick={handleDelete}>삭제</button>
             </div>
+          )}
         </div>
-    );
+
+        <Avatar className="flex">
+          <AvatarImage src={post.avatar!} alt="@profile" />
+          <AvatarFallback>
+            <div className="h-10 w-10 relative rounded-full overflow-hidden">
+              <Image
+                src={
+                  "https://ngtnbcqokvtyrilhkwpz.supabase.co/storage/v1/object/public/profile/Vector.png"
+                }
+                alt="profile"
+                fill
+                className="object-contain"
+                sizes="(max-width: 768px) 100vw, 33vw"
+              />
+            </div>
+          </AvatarFallback>
+        </Avatar>
+
+        <div className="flex flex-col card content ml-5 w-full">
+          <div className="flex items-center">
+            <h4 className="text-sm font-medium leading-none mr-2">
+              {post.nickname}
+            </h4>
+            <p className="text-sm text-muted-foreground">{post.email}</p>
+          </div>
+          <p className="text-xs text-muted-foreground mt-2">
+            {format(new Date(post.created_at), "yyyy-MM-dd HH:mm")}
+          </p>
+          <Separator className="mt-1 mb-6 border-black" />
+          <div className="flex h-5 items-center space-x-4 text-sm">
+            <p className="text-ellipsis line-clamp-3 overflow-hidden w-[80%]">
+              {post.contents}
+            </p>
+          </div>
+          <CheerupButton postId={post.id} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PostCard;
